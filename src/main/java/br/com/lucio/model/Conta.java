db33831242e5820702b8704da04e1202e7e41d64
Rodrigo Lúcio <luciodigo@gmail.com>
2022-05-10 22:32:17 -0300

    Implementando o Comparable na classe Conta

--- src/main/java/br/com/lucio/model/Conta.java
+++ src/main/java/br/com/lucio/model/Conta.java
@@ -5,10 +5,16 @@ import lombok.ToString;
 
 @Data
 @ToString
-public class Conta {
+public class Conta implements Comparable<Conta>{
 
 	private Integer numero;
 	private Integer agencia;
 	private Titular titular;
+	private Double saldo;
+	
+	@Override
+	public int compareTo(Conta conta) {
+		return Double.compare(this.saldo, conta.saldo);
+	}
 	
 }
--- src/main/java/br/com/lucio/ordenacaolistas/TesteOrdenacao.java
+++ src/main/java/br/com/lucio/ordenacaolistas/TesteOrdenacao.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import br.com.lucio.model.Conta;
 import br.com.lucio.model.Titular;
@@ -17,21 +18,25 @@ public class TesteOrdenacao {
 		cc1.setAgencia(11);
 		cc1.setNumero(111);
 		cc1.setTitular(new Titular("Zulmir"));
+		cc1.setSaldo(11111.0);
 		
 		Conta cc2 = new Conta();
 		cc2.setAgencia(22);
 		cc2.setNumero(222);
 		cc2.setTitular(new Titular("Rodrigo"));
+		cc2.setSaldo(1234332.0);
 		
 		Conta cc3 = new Conta();
 		cc3.setAgencia(33);
 		cc3.setNumero(333);
 		cc3.setTitular(new Titular("Danielly"));
+		cc3.setSaldo(333.0);
 		
 		Conta cc4 = new Conta();
 		cc4.setAgencia(44);
 		cc4.setNumero(444);
 		cc4.setTitular(new Titular("Ana"));
+		cc4.setSaldo(1.0);
 		
 		List<Conta> contas = new ArrayList<>();
 		contas.add(cc3);
@@ -39,18 +44,14 @@ public class TesteOrdenacao {
 		contas.add(cc4);
 		contas.add(cc1);
 		
-		ComparadorContas comparador = new ComparadorContas();
-		
-		contas.sort(comparador);
+		contas.sort(new ComparadorContas());
 		
 		System.out.println("Lista ordenada pelo numero da conta:");
 		contas.forEach(conta -> {
 			System.out.println(conta);
 		});
 
-		ComparadorTitular comparadorTitular = new ComparadorTitular();
-
-		contas.sort(comparadorTitular);
+		contas.sort(new ComparadorTitular());
 		System.out.println("Lista ordenada pelo nome do titular:");
 		contas.forEach(conta -> {
 			System.out.println(conta);
@@ -59,7 +60,18 @@ public class TesteOrdenacao {
 		// ou  
 		//Collections.sort(contas, Comparator.comparing(Conta::getNumero));
 		// ou
-		// Collections.sort(contas, comparador);
+		// Collections.sort(contas, new ComparadorContas());
+	
+		//Ou (só funciona pois implementei o Comparable na classe Conta, ficando a ordem "natural")
+		Collections.sort(contas);
+		
+		// também funcionaria:
+		// Collections.sort(contas, Comparator.comparing(Conta::getSaldo));
+		
+		System.out.println("Lista ordenada pela ordenação 'Natural':");
+		contas.forEach(conta -> {
+			System.out.println(conta);
+		});
 		
 	}
 	
@@ -69,16 +81,7 @@ class ComparadorContas implements Comparator<Conta> {
 
 	@Override
 	public int compare(Conta c1, Conta c2) {
-
-		if(c1.getNumero() < c2.getNumero()) {
-			return -1;
-		}
-
-		if(c1.getNumero() > c2.getNumero()) {
-			return 1;
-		}
-		
-		return 0;
+		return Integer.compare(c1.getNumero(), c2.getNumero());
 	}
 	
 }
